5c2d853925dc12a862438f737ec01a2b1a2486ee
tufronicolas <ntufro@udesa.edu.ar>
2021-04-13 12:26:04 -0300

    commit_clase_11-13abril

---
+++ clase11_intro_js/js/index.js
@@ -0,0 +1,15 @@
+console.log("hola");
+
+
+
+// asignando y reasignando con let
+let nombre = "ale";
+nombre="nico";
+console.log(nombre);
+
+// asignando y reasignando con const
+const universidad ="udesa"
+console.log(universidad);
+
+// Prueba (no funk)
+console.log('hola mi nombre es nico y voy a ${universidad}')
